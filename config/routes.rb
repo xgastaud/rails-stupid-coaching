153697efbf526dfcbd91d3af0e1273fe1e7fb0f2
Xavier Gastaud <xgastaud@gmail.com>
2018-03-05 12:47:50 +0100

    my first ROR setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'questions/ask'
+  # get 'questions/answer'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  # get 'ask', to: 'pages#ask', as: :ask
+  # get 'answer', to: 'pages#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
